dd5b3aeae2f3a5a179b24286ffcbc02efae88999
Lautaroavila <lautarojuliann@hotmail.com>
2022-01-17 21:52:30 -0300

    Agrego lazy a las rutas

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -5,40 +5,46 @@ import {
   NavLink
 } from 'react-router-dom';
 
+// import * as LazyPage from "../01-lazyload/pages";  y al llamar a los componentes: <LazyPage.LazyPage1 />
+// import { LazyPage1, LazyPage2, LazyPage3 } from '../01-lazyload/pages';
+
 import logo from '../logo.svg';
+
+import { routes } from './routes';
 
 export const Navigation = () => {
   return (
     <Router>
       <div className="main-layout">
+        
         <nav>
             <img src={ logo } alt="React Logo" />
           <ul>
-            <li>
-              <NavLink to="/" activeClassName="nav-active" exact>Home</NavLink>
-            </li>
-            <li>
-              <NavLink to="/about" activeClassName="nav-active" exact>About</NavLink>
-            </li>
-            <li>
-              <NavLink to="/users" activeClassName="nav-active" exact>Users</NavLink>
-            </li>
+            { 
+              routes.map( ({ to, name }) => (
+                <li key={to}>
+                  <NavLink to={to} activeClassName="nav-active" exact>{name}</NavLink>
+                </li>
+              )) 
+            }
           </ul>
         </nav>
 
-        {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
         <Switch>
-          <Route path="/about">
-            <h1>About</h1>
-          </Route>
-          <Route path="/users">
-            <h1>Users</h1>
-          </Route>
-          <Route path="/">
-            <h1>Home</h1>
-          </Route>
+          {
+            routes.map( ({ path, Component }) => (
+                <Route 
+                  key={path}
+                  path={path}
+                >
+                  <Component />
+                </Route>
+            )) 
+          }
+
+          {/* <Route path="/*" element={<Navigate to={routes[0].to} replace/>} /> */}
         </Switch>
+
       </div>
     </Router>
   );
